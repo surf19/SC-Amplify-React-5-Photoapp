91095e16fe7118cae9a67a4eb8544ad1fbc68bd1
Pavan <pavan.km@outlook.com>
2021-09-28 19:00:05 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+/* src/App.js */
+import React, { useState } from "react";
+import { Radio } from "antd";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Posts from "./Posts";
+import CreatePost from "./CreatePost";
 
 function App() {
+  const [viewState, updateViewState] = useState("viewPosts");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={container}>
+      <h1>Photo App</h1>
+      <Radio.Group
+        value={viewState}
+        onChange={(e) => updateViewState(e.target.value)}
+      >
+        <Radio.Button value="viewPosts">View Posts</Radio.Button>
+        <Radio.Button value="addPost">Add Post</Radio.Button>
+      </Radio.Group>
+      {viewState === "viewPosts" ? (
+        <Posts />
+      ) : (
+        <CreatePost updateViewState={updateViewState} />
+      )}
+      <AmplifySignOut />
     </div>
   );
 }
+const container = { width: 500, margin: "0 auto", padding: 50 };
 
-export default App;
+export default withAuthenticator(App);
